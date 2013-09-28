c05dd4998da9bf2cbf36ae979f95ea748392c94a
Fernando Paredes <nano@fdp.io>
2013-09-28 15:44:11 -0700

    Tests pass for notification email

--- lib/mail_form/base.rb
+++ lib/mail_form/base.rb
@@ -6,17 +6,29 @@ module MailForm
     extend ActiveModel::Translation
     include ActiveModel::Validations
 
+
     attribute_method_prefix 'clear_'
     attribute_method_suffix '?'
+    class_attribute :attribute_names
+    self.attribute_names = []
 
     def self.attributes(*names)
       attr_accessor *names
-
       define_attribute_methods(names)
+
+      self.attribute_names += names
     end
 
     def persisted?
       false
+    end
+
+    def deliver
+      if valid?
+        MailForm::Notifier.contact(self).deliver
+      else
+        false
+      end
     end
 
     protected
