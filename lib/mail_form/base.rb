c2ac5b2a25ec1a0e75409fbf7848b47247a344c1
Fernando Paredes <nano@fdp.io>
2013-09-27 17:56:51 -0700

    Make plugin compatible with Rails

--- lib/mail_form/base.rb
+++ lib/mail_form/base.rb
@@ -1,6 +1,11 @@
 module MailForm
   class Base
     include ActiveModel::AttributeMethods
+    include ActiveModel::Conversion
+    extend ActiveModel::Naming
+    extend ActiveModel::Translation
+    include ActiveModel::Validations
+
     attribute_method_prefix 'clear_'
     attribute_method_suffix '?'
 
@@ -8,6 +13,10 @@ module MailForm
       attr_accessor *names
 
       define_attribute_methods(names)
+    end
+
+    def persisted?
+      false
     end
 
     protected
